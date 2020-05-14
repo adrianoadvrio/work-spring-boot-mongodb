748bc2b0dc5064bfaa4e2ad32d611246638df9eb
adrianoadvrio <adriano@advrio.com.br>
2020-05-14 17:19:19 -0300

    conect to momgoDB with repository and service

---
+++ src/main/java/com/adrianoadvrio/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.adrianoadvrio.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adrianoadvrio.workshopmongo.domain.User;
+import com.adrianoadvrio.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	public List<User> findAll() {
+		return repo.findAll();
+		
+	}
+
+}
